c2dfc70a4aa274d2c2d0bded84b81d048ff7f13a
Iain Wood <iain@soulflyer.co.uk>
2015-10-16 18:51:26 +0700

    Initial working version

--- src/metadata_extractor_test/core.clj
+++ src/metadata_extractor_test/core.clj
@@ -4,25 +4,23 @@
            com.drew.metadata.Tag
            com.drew.imaging.ImageMetadataReader))
 
-(def testfile "/Users/iain/Pictures/Published/fullsize/2015/09/01-Dragon/IMG_6666.jpg")
-(def file (java.io.File. testfile))
+(defn gettags [taglist]
+  (let [acc {}]
+    (reduce
+     (fn [a b]
+       (assoc a (.getTagName b) (.getDescription b)))
+     acc (.getTags taglist))))
 
-file
-;; (def metadata (Metadata.))
-;; metadata
-
-(def imagedata (ImageMetadataReader/readMetadata file))
-imagedata
-
-(seq (.getDirectories imagedata))
-(map (fn [n] (.getTags n)) (.getDirectories imagedata))
-(pprint (seq (map (fn [n] (.getTags n)) (.getDirectories imagedata))))
-
-(def listtags (seq (map (fn [n] (.getTags n)) (.getDirectories imagedata))))
-listtags
-(map (fn [n] (.getTagName n)) listtags)
+(defn accumulate-tags [list tags]
+  (merge list (gettags tags)))
 
 (defn getmeta
   [file]
-  (let [imagedata (ImageMetadataReader/readMetadata file)]
-    (map (fn [dir] (.getTags dir)) (.getDirectories imagedata))))
+  (let [imagedata (ImageMetadataReader/readMetadata file)
+        acc {}]
+    (reduce accumulate-tags
+            acc (.getDirectories imagedata))))
+
+(def testfile "/Users/iain/Pictures/Published/fullsize/2015/09/01-Dragon/IMG_6666.jpg")
+(def file (java.io.File. testfile))
+(getmeta file)
