5c4eed93950acf5f2edc194e248a1a0159e19528
Iain Wood <iain@soulflyer.co.uk>
2015-10-19 00:24:49 +0700

    again added the field filtering to getmeta?????

--- src/metadata/core.clj
+++ src/metadata/core.clj
@@ -14,13 +14,15 @@
 
 (defn getmeta
   "Returns a hash-map containing the metadata from an image file.
-  Expects a java.io.File"
-  [file]
-  (let [imagedata (ImageMetadataReader/readMetadata file)
-        acc {}]
-    (reduce (fn [list tags]
-              (merge list (gettags tags)))
-            acc (.getDirectories imagedata))))
+  Expects a java.io.File and an optional sequence of field names."
+  ([file]
+   (let [imagedata (ImageMetadataReader/readMetadata file)
+         acc {}]
+     (reduce (fn [list tags]
+               (merge list (gettags tags)))
+             acc (.getDirectories imagedata))))
+  ([file fields]
+   (select-keys (getmeta file) fields)))
 
 (defn keywords
   "returns a vector of strings containing the keywords extracted from the metadata"
@@ -30,4 +32,5 @@
 ;; (def filename "/Users/iain/Pictures/Published/fullsize/2015/09/01-Dragon/IMG_6666.jpg")
 ;; (def file (java.io.File. filename))
 ;; (getmeta file)
+;; (getmeta file ["Model"])
 ;; (keywords (getmeta file))
