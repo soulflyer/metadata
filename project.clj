0866c325fdf3fb81f0cbc78e476b46373f457317
Iain Wood <iain@soulflyer.co.uk>
2015-10-18 13:12:05 +0700

    tidied up for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject metadata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Get matadata from image files"
+  :url "https://github.com/soulflyer/metadata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
